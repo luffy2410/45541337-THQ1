54d2244f691494892f8da9b6c0db7ab1b09663ee
luffy2410 <48709898+luffy2410@users.noreply.github.com>
2019-03-20 22:03:40 +1100

    Update 45541337-THQ1.R

--- 45541337-THQ1.R
+++ 45541337-THQ1.R
@@ -8,7 +8,7 @@ Coupon=C
 Face value=F
 The number of coupon payments=n
 j=seq(0.5,n,by=0.5)
-y=c(y(0.5),y(1),y(1.5),...,y(n))
+y=c(y(0.5),y(1),y(1.5),...,y(tn))
 P=sum(C*e^-(y*j))+F*e^-(y(n)*n)
 
 #Q3
